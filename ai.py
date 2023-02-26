c9b8fe2e57334fa526749df743d13e3803a932b1
JackPieper <jack.pieper@student.hu.nl>
2023-02-26 23:58:30 +0100

    ales af

--- ai.py
+++ ai.py
@@ -3,6 +3,7 @@ import spel
 
 
 def simple():
+    # Bron: Theorie van het artikel van de Universiteit Groningen en uitleg van student Jason Griffioen
     print('Simple algoritme wordt nu gespeeld')
     secret = spel.makecode()
     print(f'Code {secret} wordt nu geraden')
@@ -24,6 +25,7 @@ def simple():
 
 
 def worstcase():
+    # Bron: Theorie van het artikel van de Universiteit Groningen
     print('Worstcase algoritme wordt nu gespeeld')
     secret = spel.makecode()
     print(f'Code {secret} wordt nu geraden')
@@ -60,4 +62,37 @@ def worstcase():
     print('De ai heeft de code niet geraden.')
 
 def newAlgorithm():
-    pass
+    print('Worstcase algoritme wordt nu gespeeld')
+    secret = spel.makecode()
+    print(f'Code {secret} wordt nu geraden')
+    colors = ['A', 'B', 'C', 'D', 'E', 'F']
+    list1 = list(itertools.product(colors, repeat=4))
+    list2 = []
+    responses = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [1, 0], [1, 1], [1, 2], [1, 3], [2, 0], [2, 1], [2, 2], [3, 0]]
+    for i in range(10):
+        if i == 0:
+            guess = ['A', 'A', 'B', 'B']
+        else:
+            possibilities = {}
+            for j in list1:
+                possibilities[j] = 0
+                for k in responses:
+                    count = 0
+                    for l in list1:
+                        if spel.comp(j, l) == k:
+                            count += 1
+                    if count > possibilities[j]:
+                        possibilities[j] = count
+            guess = list(possibilities.values()).sort()[len(possibilities) / 2]
+
+        print(f'Poging {i + 1}/10: {guess}')
+        response = spel.comp(guess, secret)
+        if response[0] == 4:
+            print('De ai heeft de code geraden.')
+            return None
+        print(f'Correct: {response[0]}\nVerkeerde positie: {response[1]}\n----------')
+        for j in list1:
+            if response == spel.comp(j, guess):
+                list2.append(j)
+        list1, list2 = list2, []
+    print('De ai heeft de code niet geraden.')
