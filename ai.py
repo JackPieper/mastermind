68c7778cdd4f86629a8b2a7a099e9e54a4481804
JackPieper <jack.pieper@student.hu.nl>
2023-03-06 11:10:38 +0100

    optimize

--- ai.py
+++ ai.py
@@ -11,15 +11,8 @@ def simple():
     list1 = list(itertools.product(colors, repeat=4))
     list2 = []
     for i in range(10):
-        print(f'Poging {i + 1}/10: {list1[0]}')
-        response = spel.comp(list1[0], secret)
-        if response[0] == 4:
-            print('De ai heeft de code geraden.')
-            return None
-        print(f'Correct: {response[0]}\nVerkeerde positie: {response[1]}\n----------')
-        for j in list1:
-            if response == spel.comp(j, list1[0]):
-                list2.append(j)
+        guess = list1[0]
+        turn(guess, secret, list1, list2, i)
         list1, list2 = list2, []
     print('De ai heeft de code niet geraden.')
 
@@ -32,32 +25,13 @@ def worstcase():
     colors = ['A', 'B', 'C', 'D', 'E', 'F']
     list1 = list(itertools.product(colors, repeat=4))
     list2 = []
-    responses = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [1, 0], [1, 1], [1, 2], [1, 3], [2, 0], [2, 1], [2, 2], [3, 0]]
     for i in range(10):
         if i == 0:
             guess = ['A', 'A', 'B', 'B']
         else:
-            possibilities = {}
-            for j in list1:
-                possibilities[j] = 0
-                for k in responses:
-                    count = 0
-                    for l in list1:
-                        if spel.comp(j, l) == k:
-                            count += 1
-                    if count > possibilities[j]:
-                        possibilities[j] = count
+            possibilities = answers(list1)
             guess = min(possibilities, key=possibilities.get)
-
-        print(f'Poging {i + 1}/10: {guess}')
-        response = spel.comp(guess, secret)
-        if response[0] == 4:
-            print('De ai heeft de code geraden.')
-            return None
-        print(f'Correct: {response[0]}\nVerkeerde positie: {response[1]}\n----------')
-        for j in list1:
-            if response == spel.comp(j, guess):
-                list2.append(j)
+        turn(guess, secret, list1, list2, i)
         list1, list2 = list2, []
     print('De ai heeft de code niet geraden.')
 
@@ -69,31 +43,37 @@ def newAlgorithm():
     colors = ['A', 'B', 'C', 'D', 'E', 'F']
     list1 = list(itertools.product(colors, repeat=4))
     list2 = []
-    responses = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [1, 0], [1, 1], [1, 2], [1, 3], [2, 0], [2, 1], [2, 2], [3, 0]]
     for i in range(10):
         if i == 0:
             guess = ['A', 'A', 'B', 'B']
         else:
-            possibilities = {}
-            for j in list1:
-                possibilities[j] = 0
-                for k in responses:
-                    count = 0
-                    for l in list1:
-                        if spel.comp(j, l) == k:
-                            count += 1
-                    if count > possibilities[j]:
-                        possibilities[j] = count
+            possibilities = answers(list1)
             guess = list(j for j in possibilities if possibilities[j] == sorted(list(possibilities.values()))[int(len(possibilities) / 2)])[0]
-
-        print(f'Poging {i + 1}/10: {guess}')
-        response = spel.comp(guess, secret)
-        if response[0] == 4:
-            print('De ai heeft de code geraden.')
-            return None
-        print(f'Correct: {response[0]}\nVerkeerde positie: {response[1]}\n----------')
-        for j in list1:
-            if response == spel.comp(j, guess):
-                list2.append(j)
+        turn(guess, secret, list1, list2, i)
         list1, list2 = list2, []
     print('De ai heeft de code niet geraden.')
+
+def answers(lijst):
+    responses = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [1, 0], [1, 1], [1, 2], [1, 3], [2, 0], [2, 1], [2, 2], [3, 0]]
+    possibilities = {}
+    for j in lijst:
+        possibilities[j] = 0
+        for k in responses:
+            count = 0
+            for l in lijst:
+                if spel.comp(j, l) == k:
+                    count += 1
+            if count > possibilities[j]:
+                possibilities[j] = count
+    return possibilities
+
+def turn(guess, secret, list1, list2, i):
+    print(f'Poging {i + 1}/10: {guess}')
+    response = spel.comp(guess, secret)
+    if response[0] == 4:
+        print('De ai heeft de code geraden.')
+        return None
+    print(f'Correct: {response[0]}\nVerkeerde positie: {response[1]}\n----------')
+    for j in list1:
+        if response == spel.comp(j, guess):
+            list2.append(j)
